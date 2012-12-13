604c46aeaf62ae628ef1601a366536c555f904f3
Jairo Junior <junior.jairo1@gmail.com>
2012-12-12 23:48:23 -0300

    Some small refactors.

--- app/coin_change.rb
+++ app/coin_change.rb
@@ -1,13 +1,11 @@
 class CoinChange
 
-    def initialize
-        @avaiableCoins = [1,5,10,25,100]
-        @emptyChange = @avaiableCoins.inject({}) { |result, coin| result.merge({coin => 0})}
-    end
+    @@avaiableCoins = [1,5,10,25,100]
+    @@emptyChange = @@avaiableCoins.inject({}) { |result, coin| result.merge({coin => 0})}
 
     def change_for amount
         if amount == 0
-            @emptyChange
+            @@emptyChange
         else
             biggestChange = biggest_single_coin_change amount
             remainderValue = amount - biggestChange
@@ -19,7 +17,7 @@ class CoinChange
     private
 
     def biggest_single_coin_change amount
-        @avaiableCoins.select { |key| key <= amount}.max
+        @@avaiableCoins.select { |key| key <= amount}.max
     end
 
 end
--- test/unit/coin_change_test.rb
+++ test/unit/coin_change_test.rb
@@ -3,52 +3,48 @@ require_relative '../../app/coin_change'
 
 class CoinChangeTest < Test::Unit::TestCase
 
+    def setup
+        @coinChange = CoinChange.new
+    end
+
     def test_return_change_for_1_cents
-        coinChange = CoinChange.new
         expectedChange = {1 => 1, 5 => 0, 10 => 0, 25 => 0, 100 => 0}
-        assert_equal expectedChange, coinChange.change_for(1)
+        assert_equal expectedChange, @coinChange.change_for(1)
     end
 
     def test_return_change_for_5_cents
-        coinChange = CoinChange.new
         expectedChange = {1 => 0, 5 => 1, 10 => 0, 25 => 0, 100 => 0}
-        assert_equal expectedChange, coinChange.change_for(5)
+        assert_equal expectedChange, @coinChange.change_for(5)
     end
 
     def test_return_change_for_10_cents
-        coinChange = CoinChange.new
         expectedChange = {1 => 0, 5 => 0, 10 => 1, 25 => 0, 100 => 0}
-        assert_equal expectedChange, coinChange.change_for(10)
+        assert_equal expectedChange, @coinChange.change_for(10)
     end
 
     def test_return_change_for_25_cents
-        coinChange = CoinChange.new
         expectedChange = {1 => 0, 5 => 0, 10 => 0, 25 => 1, 100 => 0}
-        assert_equal expectedChange, coinChange.change_for(25)
+        assert_equal expectedChange, @coinChange.change_for(25)
     end
 
     def test_return_change_for_100_cents
-        coinChange = CoinChange.new
         expectedChange = {1 => 0, 5 => 0, 10 => 0, 25 => 0, 100 => 1}
-        assert_equal expectedChange, coinChange.change_for(100)
+        assert_equal expectedChange, @coinChange.change_for(100)
     end
 
     def test_return_change_for_6_cents
-        coinChange = CoinChange.new
         expectedChange = {1 => 1, 5 => 1, 10 => 0, 25 => 0, 100 => 0}
-        assert_equal expectedChange, coinChange.change_for(6)
+        assert_equal expectedChange, @coinChange.change_for(6)
     end
 
     def test_return_change_for_30_cents
-        coinChange = CoinChange.new
         expectedChange = {1 => 0, 5 => 1, 10 => 0, 25 => 1, 100 => 0}
-        assert_equal expectedChange, coinChange.change_for(30)
+        assert_equal expectedChange, @coinChange.change_for(30)
     end
 
     def test_return_change_for_111_cents
-        coinChange = CoinChange.new
         expectedChange = {1 => 1, 5 => 0, 10 => 1, 25 => 0, 100 => 1}
-        assert_equal expectedChange, coinChange.change_for(111)
+        assert_equal expectedChange, @coinChange.change_for(111)
     end
 
 end
