1ccddfe8cb1da7127855d9ca564f145b511417e7
jairojunior <junior.jairo1@gmail.com>
2012-12-21 00:22:51 -0300

    Reformated code using RubyMine.

--- app/coin_change.rb
+++ app/coin_change.rb
@@ -1,23 +1,23 @@
 class CoinChange
 
-    @@avaiable_coins = [1,5,10,25,100]
-    @@empty_change = @@avaiable_coins.inject({}) { |result, coin| result.merge({coin => 0})}
+  @@avaiable_coins = [1, 5, 10, 25, 100]
+  @@empty_change = @@avaiable_coins.inject({}) { |result, coin| result.merge({coin => 0}) }
 
-    def change_for amount
-        if amount == 0
-            @@empty_change
-        else
-            biggest_change = biggest_single_coin_change amount
-            remainder_value = amount - biggestChange
+  def change_for amount
+    if amount == 0
+      @@empty_change
+    else
+      biggest_change = biggest_single_coin_change amount
+      remainder_value = amount - biggest_change
 
-            change_for(remainderValue).merge({biggestChange => 1}) { |key, oldValue, newValue| oldValue + newValue}
-        end
+      change_for(remainder_value).merge({biggest_change => 1}) { |key, oldValue, newValue| oldValue + newValue }
     end
+  end
 
-    private
+  private
 
-    def biggest_single_coin_change amount
-        @@avaiable_coins.select { |key| key <= amount}.max
-    end
+  def biggest_single_coin_change amount
+    @@avaiable_coins.select { |key| key <= amount }.max
+  end
 
 end
--- test/unit/coin_change_test.rb
+++ test/unit/coin_change_test.rb
@@ -3,48 +3,48 @@ require_relative '../../app/coin_change'
 
 class CoinChangeTest < Test::Unit::TestCase
 
-    def setup
-        @coin_change = CoinChange.new
-    end
+  def setup
+    @coin_change = CoinChange.new
+  end
 
-    def test_change_for_1_cents
-        expected_change = {1 => 1, 5 => 0, 10 => 0, 25 => 0, 100 => 0}
-        assert_equal expected_change, @coin_change.change_for(1)
-    end
+  def test_change_for_1_cents
+    expected_change = {1 => 1, 5 => 0, 10 => 0, 25 => 0, 100 => 0}
+    assert_equal expected_change, @coin_change.change_for(1)
+  end
 
-    def test_change_for_5_cents
-        expected_change = {1 => 0, 5 => 1, 10 => 0, 25 => 0, 100 => 0}
-        assert_equal expected_change, @coin_change.change_for(5)
-    end
+  def test_change_for_5_cents
+    expected_change = {1 => 0, 5 => 1, 10 => 0, 25 => 0, 100 => 0}
+    assert_equal expected_change, @coin_change.change_for(5)
+  end
 
-    def test_change_for_10_cents
-        expected_change = {1 => 0, 5 => 0, 10 => 1, 25 => 0, 100 => 0}
-        assert_equal expected_change, @coin_change.change_for(10)
-    end
+  def test_change_for_10_cents
+    expected_change = {1 => 0, 5 => 0, 10 => 1, 25 => 0, 100 => 0}
+    assert_equal expected_change, @coin_change.change_for(10)
+  end
 
-    def test_change_for_25_cents
-        expected_change = {1 => 0, 5 => 0, 10 => 0, 25 => 1, 100 => 0}
-        assert_equal expected_change, @coin_change.change_for(25)
-    end
+  def test_change_for_25_cents
+    expected_change = {1 => 0, 5 => 0, 10 => 0, 25 => 1, 100 => 0}
+    assert_equal expected_change, @coin_change.change_for(25)
+  end
 
-    def test_change_for_100_cents
-        expected_change = {1 => 0, 5 => 0, 10 => 0, 25 => 0, 100 => 1}
-        assert_equal expected_change, @coin_change.change_for(100)
-    end
+  def test_change_for_100_cents
+    expected_change = {1 => 0, 5 => 0, 10 => 0, 25 => 0, 100 => 1}
+    assert_equal expected_change, @coin_change.change_for(100)
+  end
 
-    def test_change_for_6_cents
-        expected_change = {1 => 1, 5 => 1, 10 => 0, 25 => 0, 100 => 0}
-        assert_equal expected_change, @coin_change.change_for(6)
-    end
+  def test_change_for_6_cents
+    expected_change = {1 => 1, 5 => 1, 10 => 0, 25 => 0, 100 => 0}
+    assert_equal expected_change, @coin_change.change_for(6)
+  end
 
-    def test_change_for_30_cents
-        expected_change = {1 => 0, 5 => 1, 10 => 0, 25 => 1, 100 => 0}
-        assert_equal expected_change, @coin_change.change_for(30)
-    end
+  def test_change_for_30_cents
+    expected_change = {1 => 0, 5 => 1, 10 => 0, 25 => 1, 100 => 0}
+    assert_equal expected_change, @coin_change.change_for(30)
+  end
 
-    def test_change_for_111_cents
-        expected_change = {1 => 1, 5 => 0, 10 => 1, 25 => 0, 100 => 1}
-        assert_equal expected_change, @coin_change.change_for(111)
-    end
+  def test_change_for_111_cents
+    expected_change = {1 => 1, 5 => 0, 10 => 1, 25 => 0, 100 => 1}
+    assert_equal expected_change, @coin_change.change_for(111)
+  end
 
 end
