dc453b2173f13ed312e334e082db8ee4dc950394
jairojunior <junior.jairo1@gmail.com>
2012-12-21 13:50:14 -0300

    Added Gemfile

--- app/coin_change.rb
+++ app/coin_change.rb
@@ -1,11 +1,14 @@
 class CoinChange
 
-  @@avaiable_coins = [1, 5, 10, 25, 100]
-  @@empty_change = @@avaiable_coins.inject({}) { |result, coin| result.merge({coin => 0}) }
+  def initialize
+    @available_coins = [1, 5, 10, 25, 100]
+    @empty_change = @available_coins.inject({}) { |result, coin| result.merge({coin => 0}) }
+  end
+
 
   def change_for amount
     if amount == 0
-      @@empty_change
+      @empty_change
     else
       biggest_change = biggest_single_coin_change amount
       remainder_value = amount - biggest_change
@@ -17,7 +20,7 @@ class CoinChange
   private
 
   def biggest_single_coin_change amount
-    @@avaiable_coins.select { |key| key <= amount }.max
+    @available_coins.select { |key| key <= amount }.max
   end
 
 end
