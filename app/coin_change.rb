a266eb45a830cbae352bb8c00d1cd0ee03983324
Jairo Junior <junior.jairo1@gmail.com>
2013-02-08 12:09:53 -0300

    RubyMine 5.0 o/

--- app/coin_change.rb
+++ app/coin_change.rb
@@ -6,20 +6,20 @@ class CoinChange
   end
 
 
-  def change_for amount
+  def change_for(amount)
     if amount == 0
       @empty_change
     else
       biggest_change = biggest_single_coin_change amount
       remainder_value = amount - biggest_change
 
-      change_for(remainder_value).merge({biggest_change => 1}) { |key, oldValue, newValue| oldValue + newValue }
+      change_for(remainder_value).merge({biggest_change => 1}) { |_, oldValue, newValue| oldValue + newValue }
     end
   end
 
   private
 
-  def biggest_single_coin_change amount
+  def biggest_single_coin_change(amount)
     @available_coins.select { |key| key <= amount }.max
   end
 
